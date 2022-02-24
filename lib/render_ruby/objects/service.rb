c700af9008b22316f26d28fc26ba5f984db73912
nejdetkadir <nejdetkadir.550@gmail.com>
2022-02-25 00:33:06 +0300

    Add helper methods to service object

--- lib/render_ruby/objects/service.rb
+++ lib/render_ruby/objects/service.rb
@@ -2,5 +2,12 @@
 
 module RenderRuby
   class Service < Object
+    def auto_deploy_enabled?
+      autoDeploy.equal?('yes')
+    end
+
+    def suspended?
+      suspended.equal?('suspended')
+    end
   end
 end
